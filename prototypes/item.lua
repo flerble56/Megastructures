1a5ba705374ec71e28c264e03a5afcdf8cd069c6
flerble56 <blaise@blaiserobson.com>
2020-03-12 14:25:41 +0000

    updated recipe

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,16 +1,18 @@
 local node = electric-pole:new
 
-node.name = "Node"
-node.supply_area_distance = 10
+node.name = "node"
+node.pictures = table.deepcopy(data.raw("medium-electric-pole").pictures)
+node.supply_area_distance = 50
 node.maximum_wire_distance = 100
 node.draw_copper_wires = false
 node.draw_circuit_wires = false
+node.type = "electric-pole"
 
-local recipe = recipe.new
+local nodeRecipe = recipe:new
 
-recipe.ingredients = {{uranium-235,20},{uranium-238,20}}
-recipe.name = "Node"
+recipe.ingredients = {{"uranium-235",20},{"uranium-238",20},{"substation",30},{"steel-plate",20},{"processing-unit",20}}
+recipe.name = "node"
 recipe.result = "node"
 recipe.enabled = true
 
-data.extend(node,recipe)
+data.extend(node,nodeRecipe)
