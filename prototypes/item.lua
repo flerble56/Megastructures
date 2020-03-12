b092bc0a8ae025ba83cc925576417e0f525e996d
flerble56 <blaise@blaiserobson.com>
2020-03-12 14:38:30 +0000

    added placeholder icons, recipe to nodeRecipe

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -2,6 +2,9 @@ local node = electric-pole:new
 
 node.name = "node"
 node.pictures = table.deepcopy(data.raw("medium-electric-pole").pictures)
+--set the graphics to medium electric pole as a placeholder
+node.icon = table.deepcopy(data.raw("medium-electric-pole").icon)
+node.icon_size = table.deepcopy(data.raw("medium-electric-pole").icon_size)
 node.supply_area_distance = 50
 node.maximum_wire_distance = 100
 node.draw_copper_wires = false
@@ -10,9 +13,13 @@ node.type = "electric-pole"
 
 local nodeRecipe = recipe:new
 
-recipe.ingredients = {{"uranium-235",20},{"uranium-238",20},{"substation",30},{"steel-plate",20},{"processing-unit",20}}
-recipe.name = "node"
-recipe.result = "node"
-recipe.enabled = true
+nodeRecipe.ingredients = {{"uranium-235",20},{"uranium-238",20},{"substation",30},{"steel-plate",20},{"processing-unit",20}}
+nodeRecipe.name = "node"
+nodeRecipe.result = "node"
+nodeRecipe.enabled = true
+nodeRecipe.category = "advanced-crafting"
+nodeRecipe.energy_required = 20
+nodeRecipe.icon = table.deepcopy(data.raw("medium-electric-pole").icon)--as with node
+nodeRecipe.icon_size = table.deepcopy(data.raw("medium-electric-pole").icon_size)
 
 data.extend(node,nodeRecipe)
