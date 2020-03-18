7a0f99c1a3b72d1598e862f74ab21eb07e261f01
flerble56 <blaisereg@blaiserobson.com>
2020-03-18 16:06:57 +0000

    convrerted to inheriting from medium electric pole

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,17 +1,12 @@
-local node = electric-pole:new
+local node = table.deepcopy(data.raw.electric-pole["medium-electric-pole"])
 
 node.name = "node"
-node.pictures = table.deepcopy(data.raw("medium-electric-pole").pictures)
---set the graphics to medium electric pole as a placeholder
-node.icon = table.deepcopy(data.raw("medium-electric-pole").icon)
-node.icon_size = table.deepcopy(data.raw("medium-electric-pole").icon_size)
 node.supply_area_distance = 50
 node.maximum_wire_distance = 100
 node.draw_copper_wires = false
 node.draw_circuit_wires = false
-node.type = "electric-pole"
 
-local nodeRecipe = recipe:new
+local nodeRecipe = table.deepcopy(data.raw.recipe["medium-electric-pole"])
 
 nodeRecipe.ingredients = {{"uranium-235",20},{"uranium-238",20},{"substation",30},{"steel-plate",20},{"processing-unit",20}}
 nodeRecipe.name = "node"
@@ -19,7 +14,5 @@ nodeRecipe.result = "node"
 nodeRecipe.enabled = true
 nodeRecipe.category = "advanced-crafting"
 nodeRecipe.energy_required = 20
-nodeRecipe.icon = table.deepcopy(data.raw("medium-electric-pole").icon)--as with node
-nodeRecipe.icon_size = table.deepcopy(data.raw("medium-electric-pole").icon_size)
 
-data.extend(node,nodeRecipe)
+data:extend(node,nodeRecipe)
