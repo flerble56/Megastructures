707c17b25336fb61a40112419e1aae72819459a1
flerble56 <blaisereg@blaiserobson.com>
2020-04-04 15:50:39 +0100

    added nodeItem

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -12,11 +12,22 @@ node.maximum_wire_distance = 64
 node.draw_copper_wires = false
 node.draw_circuit_wires = false
 
+local nodeItem = table.deepcopy(data.raw["item"]["medium-electric-pole"])
+
+nodeItem.icons= {
+    {
+       icon=nodeItem.icon,
+       tint={r=1,g=0,b=0,a=0.3}
+    },
+}
+nodeItem.name = "nodeItem"
+nodeItem.place_result = "node"
+
 local nodeRecipe = table.deepcopy(data.raw.recipe["medium-electric-pole"])
 
 nodeRecipe.ingredients = {{"uranium-235",20},{"uranium-238",20},{"substation",30},{"steel-plate",20},{"processing-unit",20}}
 nodeRecipe.name = "node"
-nodeRecipe.result = "node"
+nodeRecipe.result = "nodeItem"
 nodeRecipe.enabled = true
 nodeRecipe.category = "advanced-crafting"
 nodeRecipe.energy_required = 20
