5518eafaa1b26c436c93874032a31c0d4de4287d
flerble56 <blaisereg@blaiserobson.com>
2020-06-05 11:24:13 +0100

    added deep drill

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -700,5 +700,404 @@ data:extend({
   enabled = false,
   ingredients = {{"heat-pipe", 2}, {"copper-plate", 20},{"low-density-structure", 10}},
   result = "conductive-heat-pipe"
-}
+},
+{
+  type = "recipe-category",
+  name = "drilling"
+},
+{
+  type = "item",
+  name = "uranium-drill-head",
+  icon = "__base__/graphics/icons/electric-engine-unit.png",
+  icon_size = 32,
+  subgroup = "intermediate-product",
+  order = "a[uranium-drill-head]",
+  stack_size = 50
+},
+{
+  type = "recipe",
+  name = "uranium-drill-head",
+  category = "crafting-with-fluid",
+  energy_required = 50,
+  ingredients =
+  {
+    {"uranium-238", 10},
+    {type="fluid", name= "lubricant", amount = 20},
+    {"advanced-circuit", 10},
+    {"steel-plate", 10},
+    {"iron-gear-wheel", 10}
+  },
+  result = "uranium-drill-head",
+  enabled = false
+},
+{
+  type = "assembling-machine",
+  name = "deep-drill",
+  icon = "__base__/graphics/icons/centrifuge.png",
+  icon_size = 32,
+  flags = {"placeable-neutral", "placeable-player", "player-creation"},
+  minable = {mining_time = 1, result = "deep-drill"},
+  max_health = 350,
+  corpse = "centrifuge-remnants",
+  dying_explosion = "medium-explosion",
+  resistances = {},
+  collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
+  selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+  drawing_box = {{-1.5, -2.2}, {1.5, 1.5}},
+
+  always_draw_idle_animation = true,
+  idle_animation =
+  {
+    layers =
+    {
+      -- Centrifuge A
+      {
+        filename = "__base__/graphics/entity/centrifuge/centrifuge-C.png",
+        priority = "high",
+        line_length = 8,
+        width = 119,
+        height = 107,
+        frame_count = 64,
+        shift = util.by_pixel(-0.5, -26.5),
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-C.png",
+          priority = "high",
+          scale = 0.5,
+          line_length = 8,
+          width = 237,
+          height = 214,
+          frame_count = 64,
+          shift = util.by_pixel(-0.25, -26.5)
+        }
+      },
+      {
+        filename = "__base__/graphics/entity/centrifuge/centrifuge-C-shadow.png",
+        draw_as_shadow = true,
+        priority = "high",
+        line_length = 8,
+        width = 132,
+        height = 74,
+        frame_count = 64,
+        shift = util.by_pixel(20, -10),
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-C-shadow.png",
+          draw_as_shadow = true,
+          priority = "high",
+          scale = 0.5,
+          line_length = 8,
+          width = 279,
+          height = 152,
+          frame_count = 64,
+          shift = util.by_pixel(16.75, -10)
+        }
+      },
+      -- Centrifuge B
+      {
+        filename = "__base__/graphics/entity/centrifuge/centrifuge-B.png",
+        priority = "high",
+        line_length = 8,
+        width = 78,
+        height = 117,
+        frame_count = 64,
+        shift = util.by_pixel(23, 6.5),
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-B.png",
+          priority = "high",
+          scale = 0.5,
+          line_length = 8,
+          width = 156,
+          height = 234,
+          frame_count = 64,
+          shift = util.by_pixel(23, 6.5)
+        }
+      },
+      {
+        filename = "__base__/graphics/entity/centrifuge/centrifuge-B-shadow.png",
+        draw_as_shadow = true,
+        priority = "high",
+        line_length = 8,
+        width = 124,
+        height = 74,
+        frame_count = 64,
+        shift = util.by_pixel(63, 16),
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-B-shadow.png",
+          draw_as_shadow = true,
+          priority = "high",
+          scale = 0.5,
+          line_length = 8,
+          width = 251,
+          height = 149,
+          frame_count = 64,
+          shift = util.by_pixel(63.25, 15.25)
+        }
+      },
+      -- Centrifuge A
+      {
+        filename = "__base__/graphics/entity/centrifuge/centrifuge-A.png",
+        priority = "high",
+        line_length = 8,
+        width = 70,
+        height = 123,
+        frame_count = 64,
+        shift = util.by_pixel(-26, 3.5),
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-A.png",
+          priority = "high",
+          scale = 0.5,
+          line_length = 8,
+          width = 139,
+          height = 246,
+          frame_count = 64,
+          shift = util.by_pixel(-26.25, 3.5)
+        }
+      },
+      {
+        filename = "__base__/graphics/entity/centrifuge/centrifuge-A-shadow.png",
+        priority = "high",
+        draw_as_shadow = true,
+        line_length = 8,
+        width = 108,
+        height = 54,
+        frame_count = 64,
+        shift = util.by_pixel(6, 27),
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-A-shadow.png",
+          priority = "high",
+          draw_as_shadow = true,
+          scale = 0.5,
+          line_length = 8,
+          width = 230,
+          height = 124,
+          frame_count = 64,
+          shift = util.by_pixel(8.5, 23.5)
+        }
+      }
+    }
+  },
+
+  animation =
+  {
+    layers =
+    {
+      -- Centrifuge C
+      {
+        filename = "__base__/graphics/entity/centrifuge/centrifuge-C-light.png",
+        priority = "high",
+        blend_mode = "additive", -- centrifuge
+        line_length = 8,
+        width = 96,
+        height = 104,
+        frame_count = 64,
+        shift = util.by_pixel(0, -27),
+
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-C-light.png",
+          priority = "high",
+          scale = 0.5,
+          blend_mode = "additive", -- centrifuge
+          line_length = 8,
+          width = 190,
+          height = 207,
+          frame_count = 64,
+          shift = util.by_pixel(0, -27.25)
+        }
+      },
+      -- Centrifuge B
+      {
+        filename = "__base__/graphics/entity/centrifuge/centrifuge-B-light.png",
+        priority = "high",
+        blend_mode = "additive", -- centrifuge
+        line_length = 8,
+        width = 65,
+        height = 103,
+        frame_count = 64,
+        shift = util.by_pixel(16.5, 0.5),
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-B-light.png",
+          priority = "high",
+          scale = 0.5,
+          blend_mode = "additive", -- centrifuge
+          line_length = 8,
+          width = 131,
+          height = 206,
+          frame_count = 64,
+          shift = util.by_pixel(16.75, 0.5)
+        }
+      },
+      -- Centrifuge A
+      {
+        filename = "__base__/graphics/entity/centrifuge/centrifuge-A-light.png",
+        priority = "high",
+        blend_mode = "additive", -- centrifuge
+        line_length = 8,
+        width = 55,
+        height = 98,
+        frame_count = 64,
+        shift = util.by_pixel(-23.5, -2),
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-A-light.png",
+          priority = "high",
+          scale = 0.5,
+          blend_mode = "additive", -- centrifuge
+          line_length = 8,
+          width = 108,
+          height = 197,
+          frame_count = 64,
+          shift = util.by_pixel(-23.5, -1.75)
+        }
+      }
+    }
+  },
+
+  working_visualisations =
+  {
+    {
+      effect = "uranium-glow", -- changes alpha based on energy source light intensity
+      light = {intensity = 0.6, size = 9.9, shift = {0.0, 0.0}, color = {r = 0.0, g = 1.0, b = 0.0}}
+    }
+  },
+  open_sound = { filename = "__base__/sound/machine-open.ogg", volume = 0.85 },
+  close_sound = { filename = "__base__/sound/machine-close.ogg", volume = 0.75 },
+  vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+  working_sound =
+  {
+    sound =
+    {
+      {
+        filename = "__base__/sound/assembling-machine-t2-1.ogg",
+        volume = 0.8
+      },
+      {
+        filename = "__base__/sound/assembling-machine-t2-2.ogg",
+        volume = 0.8
+      }
+    },
+    idle_sound = { filename = "__base__/sound/idle1.ogg", volume = 0.6 },
+    apparent_volume = 1.5
+  },
+  crafting_speed = 1,
+  crafting_categories = {"drilling"},
+  energy_source =
+  {
+    type = "electric",
+    usage_priority = "secondary-input",
+    emissions_per_minute = 20
+  },
+  energy_usage = "500kW",
+  module_specification =
+  {
+    module_slots = 0
+  },
+  allowed_effects = {"consumption", "speed", "productivity", "pollution"},
+  fluid_boxes =
+    {
+      {
+        production_type = "input",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = -1,
+        pipe_connections = {{ type="input", position = {0, -2} }},
+        secondary_draw_orders = { north = -1 }
+      },
+      off_when_no_fluid_recipe = true
+    },
+},
+{
+  type = "item",
+  name = "deep-drill",
+  icon = "__base__/graphics/icons/centrifuge.png",
+  icon_size = 32,
+  subgroup = "production-machine",
+  order = "f[deep-drill]",
+  place_result = "deep-drill",
+  stack_size = 10
+},
+{
+  type = "recipe",
+  name = "deep-drill",
+  energy_required = 20,
+  enabled = false,
+  ingredients =
+  {
+    {"uranium-drill-head", 10},
+    {"processing-unit", 50},
+    {"uranium-238", 50},
+    {"iron-gear-wheel", 20},
+    {"steel-plate", 20}
+  },
+  result = "deep-drill",
+  requester_paste_multiplier= 10
+},
+{
+  type = "technology",
+  name = "deep-crust-drilling",
+  icon_size = 128,
+  icon = "__base__/graphics/technology/nuclear-power.png",
+  effects =
+  {
+    {
+      type = "unlock-recipe",
+      recipe = "uranium-drill-head"
+    },
+    {
+      type = "unlock-recipe",
+      recipe = "deep-drill"
+    },
+    {
+      type = "unlock-recipe",
+      recipe = "deep-crust-drill"
+    },
+  },
+  prerequisites = {"uranium-processing","advanced-electronics","mining-productivity-4"},
+  unit =
+  {
+    ingredients =
+    {
+      {"automation-science-pack", 1},
+      {"logistic-science-pack", 1},
+      {"chemical-science-pack", 1},
+      {"production-science-pack", 1},
+      {"utility-science-pack", 1},
+      {"space-science-pack", 1}
+    },
+    time = 60,
+    count = 1000
+  },
+  order = "e-p-b-c"
+},
+{
+  type = "recipe",
+  name = "deep-crust-drill",
+  category = "drilling",
+  energy_required = 20,
+  enabled = false,
+  ingredients =
+  {
+    {type="fluid", name= "lubricant", amount = 200}
+  },
+  result_count = 5,
+  results = 
+  {
+    {"iron-ore", 1},
+    {"copper-ore", 1},
+    {"uranium-ore", 1},
+    {"stone", 1},
+    {"coal", 1}
+  },
+  requester_paste_multiplier= 10,
+  icon = "__base__/graphics/icons/kovarex-enrichment-process.png",
+  icon_size = 32,
+  subgroup = "intermediate-product"
+},
 })
